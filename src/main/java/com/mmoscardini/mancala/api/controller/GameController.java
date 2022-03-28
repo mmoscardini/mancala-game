a3ce03abbe240bd7279198de8ebd72362d38403f
Matheus Moscardini <matheus.moscardini@quintoandar.com.br>
2022-03-28 15:19:11 -0300

    setup game/{gameId}/{pitId} put method, and dynamic board generation

--- src/main/java/com/mmoscardini/mancala/api/controller/GameController.java
+++ src/main/java/com/mmoscardini/mancala/api/controller/GameController.java
@@ -1,10 +1,15 @@
 package com.mmoscardini.mancala.api.controller;
 
+import com.mmoscardini.mancala.core.board.Board;
+import com.mmoscardini.mancala.core.gameplayController.GameplayController;
+import com.mmoscardini.mancala.core.gameplayController.GameplayControllerFactory;
+import com.mmoscardini.mancala.core.pit.Pit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class GameController {
@@ -20,14 +25,38 @@ public class GameController {
     @GetMapping("/game/{gameId}")
     public String gamePlayPage(@PathVariable String gameId, Model model) {
         model.addAttribute("gameId", gameId);
-//        Pit pit = new Pit(6);
-//        Pit bigpit = new Pit(0);
-//        Pit[] pits = {pit, pit, pit, pit, pit, pit};
-//
-//        model.addAttribute("p1_pits", pits);
-//        model.addAttribute("p2_pits", pits);
-//        model.addAttribute("p1_bigpit", bigpit);
-//        model.addAttribute("p2_bigpit", bigpit);
+
+
+        GameplayController gameplayController = GameplayControllerFactory.getGameplayController(gameId);
+        gameplayController.setup();
+
+        Board board = gameplayController.getBoard();
+        Pit[] p1Pits = {board.getPit(1), board.getPit(2), board.getPit(3), board.getPit(4), board.getPit(5), board.getPit(6)};
+        Pit[] p2Pits = {board.getPit(7), board.getPit(8), board.getPit(9), board.getPit(10), board.getPit(11), board.getPit(12)};
+
+        System.out.println("pit1 " + board.getBigPit(1));
+
+
+        model.addAttribute("p1_pits", p1Pits);
+        model.addAttribute("p2_pits", p2Pits);
+        model.addAttribute("p1_bigpit", board.getBigPit(1));
+        model.addAttribute("p2_bigpit", board.getBigPit(2));
+        return "game";
+    }
+
+    @PutMapping("/game/{gameId}/{pitId}")
+    public String makeMove(@PathVariable String gameId, @PathVariable Integer pitId, Model model) {
+        GameplayController gameplayController = GameplayControllerFactory.getGameplayController(gameId);
+        gameplayController.setup();
+
+        Board board = gameplayController.getBoard();
+        Pit[] p1Pits = {board.getPit(1), board.getPit(2), board.getPit(3), board.getPit(4), board.getPit(5), board.getPit(6)};
+        Pit[] p2Pits = {board.getPit(7), board.getPit(8), board.getPit(9), board.getPit(10), board.getPit(11), board.getPit(12)};
+
+        model.addAttribute("p1_pits", p1Pits);
+        model.addAttribute("p2_pits", p2Pits);
+        model.addAttribute("p1_bigpit", board.getBigPit(1));
+        model.addAttribute("p2_bigpit", board.getBigPit(2));
         return "game";
     }
 }
